a5a53630d3ba65d03dd6d8d72fcbbccaf353b28e
Luke <lwestbury.63740@farnborough.ac.uk>
2018-01-26 12:15:57 +0000

    lol

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -14,63 +16,45 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Pane root = new Pane();
+        BorderPane root = new BorderPane();
+
+        VBox leftSide = new VBox();
+        leftSide.setPrefWidth(200);
+        HBox bottomSide = new HBox();
+        bottomSide.getStyleClass().add("bottomSide");
+        bottomSide.setPrefHeight(180);
+
+        root.setLeft(leftSide);
+        root.setBottom(bottomSide);
 
         Scene scene = new Scene(root, 1024, 768);
-
-        stage.setTitle("Hello World");
+        stage.setTitle("iMusic");
         stage.setScene(scene);
         stage.setResizable(false);
+        scene.getStylesheets().add("stylesheet.css");
         stage.show();
 
-        BorderPane borderPane = new BorderPane();
+        Button playButton = new Button("Play");
+        playButton.setPrefSize(50, 50);
+        playButton.getStyleClass().add("button");
+        playButton.setOnAction((ActionEvent ae) -> playPop(ae));
+        bottomSide.getChildren().add(playButton);
 
-        VBox leftPane = new VBox(20);
-        Button leftButton1 = new Button("I am left.");
-        leftPane.getChildren().add(leftButton1);
-        Button leftButton2 = new Button("I am left again.");
-        leftPane.getChildren().add(leftButton2);
-        borderPane.setLeft(leftPane);
-        leftPane.setAlignment(Pos.CENTER);
-        BorderPane.setAlignment(leftPane, Pos.CENTER_LEFT);
-
-        VBox rightPane = new VBox(20);
-        Button rightButton1 = new Button("I am right.");
-        rightPane.getChildren().add(rightButton1);
-        Button rightButton2 = new Button("I am right again.");
-        rightPane.getChildren().add(rightButton2);
-        borderPane.setRight(rightPane);
-        rightPane.setAlignment(Pos.CENTER);
-        BorderPane.setAlignment(rightPane, Pos.CENTER_RIGHT);
-
-        VBox topPane = new VBox(20);
-        Button topButton1 = new Button("I am top.");
-        topPane.getChildren().add(topButton1);
-        Button topButton2 = new Button("I am top again.");
-        topPane.getChildren().add(topButton2);
-        borderPane.setTop(topPane);
-        topPane.setAlignment(Pos.CENTER);
-        BorderPane.setAlignment(topPane, Pos.TOP_CENTER);
-
-        VBox bottomPane = new VBox(20);
-        Button bottomButton1 = new Button("I am bottom.");
-        bottomPane.getChildren().add(bottomButton1);
-        Button bottomButton2 = new Button("I am bottom again.");
-        bottomPane.getChildren().add(bottomButton2);
-        borderPane.setBottom(bottomPane);
-        bottomPane.setAlignment(Pos.CENTER);
-        BorderPane.setAlignment(bottomPane, Pos.BOTTOM_CENTER);
-
-        VBox centerPane = new VBox(20);
-        Button centerButton1 = new Button("I am centre.");
-        centerPane.getChildren().add(centerButton1);
-        Button centerButton2 = new Button("I am centre again.");
-        centerPane.getChildren().add(centerButton2);
-        borderPane.setCenter(centerPane);
-        centerPane.setAlignment(Pos.CENTER);
-        BorderPane.setAlignment(centerPane, Pos.CENTER);
-
+        ListView listPlaylists = new ListView();
+        listPlaylists.setPrefSize(200, 600);
+        listPlaylists.getStyleClass().add("listPlaylist-view");
+        leftSide.getChildren().add(listPlaylists);
     }
+
+
+    public static void playPop(ActionEvent ae) {
+        Alert playAlert = new Alert(Alert.AlertType.INFORMATION);
+        playAlert.setTitle("Hello Title");
+        playAlert.setHeaderText("Header");
+        playAlert.setContentText("Wow, Hello!!");
+        playAlert.showAndWait();
+    }
+
 
     public static void main(String[] args) {
         launch(args);
